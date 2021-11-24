355d0d4442a8d1db9c423e62bbb17d7289862c0c
Alex Bajan <alex.i.bajan@gmail.com>
2021-11-24 23:35:08 +0200

    a

--- next-test.js
+++ next-test.js
@@ -1,115 +1,13 @@
-const { Cluster } = require('puppeteer-cluster');
+const puppeteer = require('puppeteer');
+const { takeScreenshot } = require('./lib/helpers');
+
 (async () => {
-  //Create cluster with 10 workers
-  const cluster = await Cluster.launch({
-    concurrency: Cluster.CONCURRENCY_CONTEXT,
-    maxConcurrency: 30,
-    monitor: true,
-    timeout: 500000,
-    puppeteerOptions: {
-      slowMo: 50,
-      headless: true,
-    },
-  });
+  const browser = await puppeteer.launch({ headless: true, slowMo: false });
+  const page = await browser.newPage();
 
-  // Print errors to console
-  cluster.on('taskerror', (err, data) => {
-    console.log(`Error crawling ${data}: ${err.message}`);
-  });
+  await page.goto('https:/example.com');
+  await page.waitForTimeout(3000);
+  await takeScreenshot(page);
 
-  // Dumb sleep function to wait for page load
-  async function timeout(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
-
-  await cluster.task(async ({ page, data: url, worker }) => {
-    // const browser = await puppeteer.launch({ headless: false, slowMo: 50 });
-    // const page = await browser.newPage();
-
-    const messengerFrameSelector = '#web-messenger-container';
-    const messengerBubbleSelector = '#messenger-button';
-    const messengerInputSelector = '#footer > form > div > textarea';
-    const clickDelay = 3000;
-    const waitTimeout = 5000;
-
-    const waitForResponse = async (frame, response) => {
-      await frame.waitForFunction(
-        `document.querySelector("body").innerText.includes("${response}")`
-      );
-      return;
-    };
-
-    const sendMessage = async (inputSelector, message) => {
-      await inputSelector.type(message);
-      await inputSelector.press('Enter');
-      return;
-    };
-
-    const clickButton = async (frame, button) => {
-      const buttonSelector = await messengerFrame.waitForSelector(
-        `#conversation > div.messages-container > div > div.reply-container > button:nth-child(${button}) > span`
-      );
-      await buttonSelector.click();
-    };
-
-    async function timeout(ms) {
-      return new Promise(resolve => setTimeout(resolve, ms));
-    }
-
-    await page.goto(url);
-    await page.setViewport({ width: 1024, height: 768 });
-    let frames = await page.frames();
-    await page.waitForSelector(messengerFrameSelector, {
-      timeout: waitTimeout,
-    });
-    const messengerFrameContainer = await page.$(messengerFrameSelector);
-    const messengerFrame = await messengerFrameContainer.contentFrame();
-    const messengerBubble = await messengerFrame.waitForSelector(
-      messengerBubbleSelector,
-      {
-        timeout: waitTimeout,
-      }
-    );
-    await messengerBubble.click();
-    let messageInput = await messengerFrame.waitForSelector(
-      messengerInputSelector,
-      {
-        timeout: waitTimeout,
-      }
-    );
-
-    await sendMessage(messageInput, 'Hello');
-    await waitForResponse(messengerFrame, 'What do you think?');
-    await clickButton(messengerFrame, 1);
-    await waitForResponse(messengerFrame, 'What should l call you?');
-    await sendMessage(messageInput, 'Tony Harrison');
-    await clickButton(messengerFrame, 2);
-
-    // Create an array of known messenger responses to react to
-    responses = [
-      'hear about the features?',
-      'Sounds pretty cool, huh?',
-      'Still following?',
-      'integrating social channels',
-      'AI capabilities?',
-      'following along alright?',
-      'Sound good?',
-      'a sneaky feature?',
-      'competitors basic bots!',
-    ];
-
-    for (const response of responses) {
-      await waitForResponse(messengerFrame, response);
-      await clickButton(messengerFrame, 1);
-    }
-
-    await page.screenshot({ path: `screenshot${worker.id}.png` });
-    await page.screenshot({ path: 'screenshot.png' });
-  });
-
-  for (let i = 1; i <= 30; i++) {
-    cluster.queue('https://stackchat.com/');
-  }
-  await cluster.idle();
-  await cluster.close();
+  await browser.close();
 })();
